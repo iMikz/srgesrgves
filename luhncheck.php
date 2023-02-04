dfdb0b86ad1857799df227c3c14c41afafdd9caf
iMikz <89140339+iMikz@users.noreply.github.com>
2023-02-04 11:58:56 +0800

    Update luhncheck.php

--- luhncheck.php
+++ luhncheck.php
@@ -42,18 +42,6 @@ function isValid4(string $number): bool
 
     return $sum % 10 === 0;
 }
-function isValid5(string $number): bool
-{
-    $sum = 0;
-    $flag = 0;
-
-    for ($i = strlen($number) - 1; $i >= 0; $i--) {
-        $add = $flag++ & 1 ? $number[$i] * 2 : $number[$i];
-        $sum += $add > 9 ? $add - 9 : $add;
-    }
-
-    return $sum % 10 === 0;
-}
 if(isValid5("4111111111111111") == true){
 	echo "trueeeeeee";
 }else{
